73bf61444de0c964cd25fc4813db086fcffc26e0
KrishnaSharma162 <83661875+KrishnaSharma162@users.noreply.github.com>
2021-05-19 11:12:43 +0530

    Create UseOfConstant.c

---
+++ UseOfConstant.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+int main() {
+   const int a;
+   const int b = 12;
+   printf("The default value of variable a : %d", a);
+   printf("\nThe value of variable b : %d", b);
+   return 0;
+}
