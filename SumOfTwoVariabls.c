e1b6882be9abe5bcd1da5434540f3b07500dcdbc
KrishnaSharma162 <83661875+KrishnaSharma162@users.noreply.github.com>
2021-05-19 11:10:41 +0530

    Create SumOfTwoVariabls.c

---
+++ SumOfTwoVariabls.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+{
+    int var1;
+    var1=20;
+    int var2;
+    var2=30;
+    
+    int var3;
+    var3=var1+var2;
+    printf(" Sum of two variables = %d", var3);
+    return 0;
+}
