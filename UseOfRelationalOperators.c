249f0db4825d986d0f45c8188d028abf3885ea85
KrishnaSharma162 <83661875+KrishnaSharma162@users.noreply.github.com>
2021-05-20 15:14:03 +0530

    Create UseOfRelationalOperators.c

---
+++ UseOfRelationalOperators.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	
+	printf("enter the first number\n ");
+	scanf("%d",&a);	
+	
+	printf("enter the second number\n ");
+	scanf("%d",&b);
+	
+	printf("a>b=%d\n",a>b);
+	printf("a>=b=%d\n",a>=b);
+	printf("a<b=%d\n",a<b);
+	printf("a<=b=%d\n",a<=b);
+	printf("a>b=%d\n",a>b);
+	printf("a==b=%d\n",a==b);
+	printf("a!=b=%d\n",a!=b);
+			
+		return 0 ;
+	
+}
