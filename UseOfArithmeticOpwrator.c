93cbf41a81972431af45030217bf0c26f144e276
KrishnaSharma162 <83661875+KrishnaSharma162@users.noreply.github.com>
2021-05-20 14:57:03 +0530

    Create UseOfArithmeticOpwrator.c

---
+++ UseOfArithmeticOpwrator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() 
+{
+   int a=35;
+   int b=3;
+
+   printf("sub=%d\n",a-b);
+   
+      printf("sum=%d\n,a+b");
+      
+        printf("multiply=%d\n",a*b); 
+		 printf("divide=%d\n",a/b); 
+		  printf("remainder=%d\n",a%b);
+   
+
+ 
+   return 0;
+}
