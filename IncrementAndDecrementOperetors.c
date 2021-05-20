19279b79a65ea8091b03bd7027c962fbd08fddb2
KrishnaSharma162 <83661875+KrishnaSharma162@users.noreply.github.com>
2021-05-20 15:05:01 +0530

    Create IncrementAndDecrementOperetors.c

---
+++ IncrementAndDecrementOperetors.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a=10;
+	
+		printf("this code is for increament decreament program\n" );
+	
+	printf("number before the increament =%d\n",a);
+	a++;
+		printf("number after the increament =%d\n",a);
+	a--;
+			printf("number after the decrament =%d",a);
+		return 0 ;
+	
+}
