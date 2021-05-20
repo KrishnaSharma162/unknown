4e489bc91039da4915fd4d19a4cacd535af2c056
KrishnaSharma162 <83661875+KrishnaSharma162@users.noreply.github.com>
2021-05-20 15:09:28 +0530

    Create PrePostIncrementOperator.c

---
+++ PrePostIncrementOperator.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int a=10;
+	
+		printf("this code is for increament decreament program\n" );
+	
+	printf("number before the increament =%d\n",a);
+	 
+	
+		printf("number after the  pre increament =%d\n",a++);
+	
+			printf("number after the increament =%d\n",a);
+			
+			a++;
+			printf("number after the post increament =%d\n",a);
+			
+			printf("number after the increament =%d\n",a);
+			
+
+			
+		return 0 ;
+	
+}
