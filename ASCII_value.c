dbcc1f6f65231eb98fc5c37277da7d6aa35cdb7c
KrishnaSharma162 <83661875+KrishnaSharma162@users.noreply.github.com>
2021-07-24 10:37:29 +0530

    ASCII_value.c

---
+++ ASCII_value.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	char num1, num2;
+	num1= 'a';
+	num2='t';
+	
+	printf("The ASCII value of entered character is \n");
+	printf("%d\n%d\n",num1,num2);
+	 
+	 if(num1>num2)
+	 {
+	printf("The ASCII value num1=%d\n",num1);
+	 }
+	else
+	 {
+	printf("The ASCII value num2=%d\n",num2);
+	 }
+	
+			printf(" THANK YOU\n");
+
+			
+		return 0 ;
+	
+}
