eaa712271fbba29376fe3342384d763630e66deb
KrishnaSharma162 <83661875+KrishnaSharma162@users.noreply.github.com>
2021-07-24 10:36:53 +0530

    use_of_if-elseif-else.c

---
+++ use_of_if-elseif-else.c
@@ -0,0 +1,15 @@
+#include <stdio.h>  
+int main()  
+{  
+   int a=12, b=12;  
+   if(a>b){
+   	printf("a is a greater number");
+   }
+   else if(a==b){
+   	printf("Both are equal number");
+   }
+   else{
+   	printf("b is a greater number");
+   }
+   return 0;  
+}  
