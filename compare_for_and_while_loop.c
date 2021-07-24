f6ff1bc8559ac8d2674eb435ebde972908fd8944
KrishnaSharma162 <83661875+KrishnaSharma162@users.noreply.github.com>
2021-07-24 10:40:44 +0530

    compare_for_and_while_loop.c

---
+++ compare_for_and_while_loop.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+ int i;
+ printf("This is designed by Avi Goyal  ");
+ printf("In for loop");
+ for(i=0; i<=7; i++)
+ {
+   printf("The value is %d \n",i);
+ }
+ 
+ printf("In while loop\n");
+ int j=0;
+ while(j<=5)
+ {
+   printf(" %d \n",j);
+   j=j+1;
+ }
+	return 0;
+}
