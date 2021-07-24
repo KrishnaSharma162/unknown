a1e6f4d39d4d5d55804d49b742df09075a68e345
KrishnaSharma162 <83661875+KrishnaSharma162@users.noreply.github.com>
2021-07-24 10:36:15 +0530

    use_of_if-else.c

---
+++ use_of_if-else.c
@@ -0,0 +1,12 @@
+#include <stdio.h>  
+int main()  
+{  
+   int a=9, b=12;  
+   if(a>b){
+   	printf("a is a greater number");
+   }
+   else{
+   	printf("b is a greater number");
+   }
+   return 0;  
+}  
