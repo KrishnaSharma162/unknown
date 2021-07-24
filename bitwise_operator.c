dab7ac29a9946855bacfcb4825a8a7be61feb9be
KrishnaSharma162 <83661875+KrishnaSharma162@users.noreply.github.com>
2021-07-24 10:32:42 +0530

    bitwise_operator.c

---
+++ bitwise_operator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>  
+int main()  
+{  
+   int a=5, b=9;  
+   printf("The output of the Bitwise AND operator a&b is %d",a&b);  
+   printf("\nThe output of the Bitwise OR operator a|b is %d",a|b);  
+   printf("\nThe output of the Bitwise XOR operator a^b is %d",a^b);  
+   return 0;  
+}  
