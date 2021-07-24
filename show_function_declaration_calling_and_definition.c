241c1e8f306b2339bfe5302529e7d4218d8e1f8f
KrishnaSharma162 <83661875+KrishnaSharma162@users.noreply.github.com>
2021-07-24 10:44:39 +0530

    show_function_declaration_calling_and_definition.c

---
+++ show_function_declaration_calling_and_definition.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void message(); /*function prototype declaration*/
+int main()
+{
+ message(); /*function call*/
+ printf("This is designed by Avi Goyal \n");
+ printf("Cry,and you stop monotony! \n");
+ return 0;
+}
+ 
+void message() /*function definition*/
+{
+ printf("Smile,and the world smiles with you....\n");
+}
